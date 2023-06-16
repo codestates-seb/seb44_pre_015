823782b02bd7397149a254cd9416dc5fd3d29291
jieun419 <crg1050@naver.com>
2023-06-16 17:05:41 +0900

    [add]:Tag 컴포넌트 작업

---
+++ client/src/components/common/tag/tags.jsx
@@ -0,0 +1,11 @@
+import { TagContainer, Tag } from "./tags.styled"
+
+export default function Tags(){
+  return(
+    <TagContainer>
+      <Tag>React</Tag>
+      <Tag>JavaScript</Tag>
+      <Tag>CSS</Tag>
+    </TagContainer>    
+  )
+}
---
+++ client/src/components/common/tag/tags.styled.jsx
@@ -0,0 +1,15 @@
+import tw from "tailwind-styled-components";
+
+export const TagContainer = tw.div`
+  flex
+  gap-1
+  transition duration-300 ease-in-out
+`
+export const Tag = tw.span`
+  bg-[#F7F7F7]
+  text-xs
+  text-black
+  rounded-full
+  px-2
+  py-1
+`
