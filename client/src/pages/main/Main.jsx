4c6f016b6c4843ec4359547a04c4d6ef1f97d677
jieun419 <crg1050@naver.com>
2023-06-19 17:46:40 +0900

    [style]:Main.jsx 테스트한 내용 지우기 Main.jsx에서 테스트한 내용 삭제했습니다.

--- client/src/pages/main/Main.jsx
+++ client/src/pages/main/Main.jsx
@@ -2,12 +2,11 @@ import { MainContainer } from '../main/Main.styled'
 import SearchBox from '../../components/search/SearchBox'
 import FilterBtn from '../../components/filter/FilterBtn'
 import MainPostCard from '../../components/card/MainPostCard'
-import InputSection from "../../components/question/input-section/InputSection"
 
 export default function Main() {
   return (
     <MainContainer>
-      <InputSection/>
+      
       <SearchBox />
       <FilterBtn />
       
