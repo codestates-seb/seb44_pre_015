541704416dafbe77c8f2b84b875682226390ba32
ddaeunbb <ddaeunbb@gmail.com>
2023-06-19 16:44:36 +0900

    [add]: 질문페이지 대댓글 섹션 완성 질문페이지에 대댓글 섹션을 완성하였습니다.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,14 @@ import Header from "./components/common/Header";
 import Footer from "./components/common/Footer";
 import Login from './pages/login/Login'
 import Main from './pages/main/Main'
+import RecommentSection from './components/question/recomment-section/RecommentSection';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Header />
+
+      <RecommentSection />
       
       <Routes>
         <Route path='/' element={<Main />} />
