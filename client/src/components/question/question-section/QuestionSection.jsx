4a67b864bc1a1fecdd53b2f22a4ac141cffd2795
yeon0226 <w01086248649@gmail.com>
2023-06-19 18:19:51 +0900

    [add] 질문 View 페이지 - 질문섹션 구현 질문 view 페이지 중 질문섹션은 question폴더에 question-section폴더를 생성하고 QuestionSection 파일에 작업하였습니다.

---
+++ client/src/components/question/question-section/QuestionSection.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import UserInfoOwner from '../../user/UserInfoOwner';
+import EditBtn from '../../button/edit/EditBtn';
+import ReMoveBtn from '../../button/remove/ReMoveBtn';
+import Tags from '../../tag/Tags';
+import HeartBtn from '../../button/like/HeartBtn';
+import {
+  EditContainer,
+  EditDiv,
+  Heart,
+  Horizontal,
+  QuestionContainer,
+  User,
+  Title
+} from './QuestionSection.styled';
+
+export default function QuestionSection() {
+  return (
+    <EditContainer>
+      <EditDiv> 
+        <EditBtn />
+        <ReMoveBtn />
+      </EditDiv>
+
+      <div className="flex items-center">
+        <Heart>
+          <HeartBtn />
+        </Heart>
+        <User>
+          <UserInfoOwner />
+        </User>
+      </div>
+
+      <QuestionContainer>
+        <Title>Clear Image Reader Buffer in CameraX API</Title>
+        <Tags />
+      </QuestionContainer>
+
+      <Horizontal />
+      
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, dolore impedit sequi exercitationem molestias deserunt quia ab nostrum quaerat, ratione dolorem sunt placeat quo obcaecati. Laborum vel sunt quas nihil!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, dolore impedit sequi exercitationem molestias deserunt quia ab nostrum quaerat, ratione dolorem sunt placeat quo obcaecati. Laborum vel sunt quas nihil!
+      </div>
+    </EditContainer>
+  );
+}
