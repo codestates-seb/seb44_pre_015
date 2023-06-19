c5672fb3b6bb16370c3f4f230a8a31fbf8788b26
jieun419 <crg1050@naver.com>
2023-06-19 17:34:35 +0900

    [style]:InputSection 컴포넌트 구현 및 코드 수정 InputSection 컴포넌트 구현 및 코드 정리했습니다.

--- client/src/components/question/input-section/InputSection.jsx
+++ client/src/components/question/input-section/InputSection.jsx
@@ -1,9 +1,23 @@
 import UserInfoOther from "../../user/UserInfoOther"
+import { InputFormContainer, AnswerForm, AnswerTextarea, AnswerBox, AnswerButton } from "./InputSection.styled"
 
-export default function InputSection(){
-  return(
-    <>
+const handlePaintAnswer = (event) => {
+  event.preventDefault()
+}
+
+export default function InputSection() {
+  return (
+    <InputFormContainer>
+
       <UserInfoOther />
-    </>
+
+      <AnswerForm onSubmit={handlePaintAnswer}>
+        <AnswerTextarea name="answer" placeholder="답변을 작성해 주세요."></AnswerTextarea>
+        <AnswerBox>
+          <AnswerButton>등록</AnswerButton>
+        </AnswerBox>
+      </AnswerForm>
+
+    </InputFormContainer>
   )
 }
--- client/src/components/question/input-section/InputSection.styled.jsx
+++ client/src/components/question/input-section/InputSection.styled.jsx
@@ -1,1 +1,53 @@
 import tw from "tailwind-styled-components";
+
+export const InputFormContainer = tw.section`
+  w-full
+  flex
+  flex-col
+  gap-3
+`
+
+export const AnswerForm = tw.form`
+  flex
+  flex-col
+  border-[#DCDCDC]
+  border-solid
+  border-[0.1rem]
+`
+
+export const AnswerTextarea = tw.textarea`
+  bg-white
+  text-sm
+  resize-none
+  p-3
+  w-full
+  outline-0
+  h-[12rem]
+`
+
+export const AnswerBox = tw.div`
+  flex
+  justify-end
+  border-t-[0.1rem]
+  border-[#DCDCDC]
+  border-solid
+  p-3
+`
+
+export const Button = tw.button`
+  text-sm
+  text-white
+  rounded-full
+  px-6
+  py-2
+  transition duration-300 ease-in-out
+  max-md:text-xs
+`;
+
+export const AnswerButton = tw(Button)`
+  bg-[#FFA564]  
+  border-solid
+  border-[0.1rem]
+  border-[#FF7E22]
+  hover:bg-[#FF7E22]
+`
