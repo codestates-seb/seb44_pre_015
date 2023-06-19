f91b8a2d1866bfef5069460179ac9fc1cb11f086
hyeongu1 <gusrn556111@gmail.com>
2023-06-19 15:20:00 +0900

    [add]: 하트버튼,수정버튼,삭제버튼 추가 하트버튼 누를때 숫자 올라가고 한번 더 누르며 내려가게 구현 수정,삭제 호버시 밑줄

---
+++ client/src/components/button/EditBtn.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import { EditButtonWrap } from "./EditBtn.style";
+
+export default function EditButton() {
+  return (
+    <EditButtonWrap className="text-xs">수정</EditButtonWrap>
+  );
+}
---
+++ client/src/components/button/ReMoveBtn.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import { RemoveButtonWrap } from "./RemoveBtn.styled";
+
+export default function ReMoveButton() {
+  return (
+    <RemoveButtonWrap className="text-xs">삭제</RemoveButtonWrap>
+  );
+}
