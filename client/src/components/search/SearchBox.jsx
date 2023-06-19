254451a1f3b99079e9f2cdd281fe69ac03939643
ddaeunbb <ddaeunbb@gmail.com>
2023-06-19 10:26:48 +0900

    [fix]: Pull request conflict 수정 pull request 했을 시에, node_modules에서 충돌이 일어나 만든 컴포넌트를 feat-main 브렌치에 직접 주입하여 수정하였습니다.

---
+++ client/src/components/search/SearchBox.jsx
@@ -0,0 +1,13 @@
+import { AiOutlineSearch } from "react-icons/ai";
+import {MainSearchIconWrap, CenteredContainer, MainSearchContainer, MainSearchInput } from "./MainSearch.style";
+
+export default function SearchBox(){
+    return (
+    <CenteredContainer>
+      <MainSearchContainer>
+       <MainSearchIconWrap><AiOutlineSearch /></MainSearchIconWrap><MainSearchInput type="text" placeholder="질문을 검색해 보세요" />
+      </MainSearchContainer>
+      </CenteredContainer>
+    );
+  };
+  
