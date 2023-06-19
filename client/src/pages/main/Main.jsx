b05b220b3130581ca0f38f5b93b5a65d620f6925
jieun419 <crg1050@naver.com>
2023-06-19 16:23:42 +0900

    [add]:InputSection, UserInfoOther 파일 생성 InputSection에 필요한 기본 파일 1차 셋팅

--- client/src/pages/main/Main.jsx
+++ client/src/pages/main/Main.jsx
@@ -2,10 +2,12 @@ import { MainContainer } from '../main/Main.styled'
 import SearchBox from '../../components/search/SearchBox'
 import FilterBtn from '../../components/filter/FilterBtn'
 import MainPostCard from '../../components/card/MainPostCard'
+import InputSection from "../../components/question/input-section/InputSection"
 
 export default function Main() {
   return (
     <MainContainer>
+      <InputSection />
       <SearchBox />
       <FilterBtn />
       
