503f2be9d90f58a307928cb42caa5ca3da1c4768
ddaeunbb <ddaeunbb@gmail.com>
2023-06-19 09:34:10 +0900

    [fix]: App.jsx 필요없는 모듈 코드 픽스 selection 컴포넌트를 만들고 실험하였던 모듈코드를 삭제하였습니다.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/common/Header";
 import Footer from "./components/common/Footer";
 import LoginBox from "./components/login/LoginBox";
-import Selection from "./components/selectiton/Selection";
 
 export default function App() {
   return (
