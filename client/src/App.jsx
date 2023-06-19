8792158c61b81fe79a7c4387e8c392cd97aa88ce
hyeongu1 <gusrn556111@gmail.com>
2023-06-19 19:25:12 +0900

    [refactor]: app건든부분 수정

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import CommentSection from "./components/question/comment-section/commentsection";
 import Header from "./components/common/Header";
 import Footer from "./components/common/Footer";
 import Login from './pages/login/Login'
@@ -9,7 +8,7 @@ export default function App() {
   return (
     <BrowserRouter>
       <Header />
-      <CommentSection></CommentSection>
+      
       <Routes>
         <Route path='/' element={<Main />} />
         <Route path="/login" element={<Login />} />
