e4841acb5fc51428fb53d08355249c590af423e6
hyeongu1 <gusrn556111@gmail.com>
2023-06-19 21:24:20 +0900

    [add]:질문작성페이지 제목 추가 질문작성 추가 오랜지색 서클 추가 텍스트 추가

---
+++ client/src/components/button/question/QuestionWriteHead.styled.jsx
@@ -0,0 +1,31 @@
+import tw from "tailwind-styled-components";
+
+export const QuestionWrap = tw.div`
+    flex
+    ml-2
+    mt-2
+`;
+
+export const QuestionHeadWrap = tw.div`
+    flex
+    flex-col
+`;
+
+export const QuestionHead = tw.p`
+    text-xl  
+    font-bold
+`;
+
+export const OrangeCircle = tw.div `
+    w-2
+    h-2
+    border-solid
+    bg-orange-500
+    rounded-full
+    mt-1
+`;
+
+export const QuestionHeadtext = tw.p`
+    text-xs
+    text-slate-500
+`;
