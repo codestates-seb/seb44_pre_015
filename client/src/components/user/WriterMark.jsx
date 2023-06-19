cb558a27321847ffded5a9686a1035379e5536a0
yeon0226 <w01086248649@gmail.com>
2023-06-19 14:49:56 +0900

    [add] : 작성자 댓글 컴포넌트 본인이 작성한 글에 대한 작성자 표시하는 버튼타입의 컴포넌트 생성

---
+++ client/src/components/user/WriterMark.jsx
@@ -0,0 +1,8 @@
+import tw from 'tailwind-styled-components';
+import { Markshape } from './WriterMark.styled';
+
+export default function WriterBtn() {
+  return (
+    <Markshape>질문주인</Markshape>
+  );
+}
