cd6eac8394f7b76a57caf84bc9d9599dbd8a9e27
jieun419 <crg1050@naver.com>
2023-06-20 15:06:40 +0900

    [add]:파일 셋팅 마이페이지 유저 정보 관련 파일 셋팅

---
+++ client/src/components/user/UserInfoMy.jsx
@@ -0,0 +1,13 @@
+import { UserImg, Img } from "./UserInfoMyp.styled"
+
+export default function UserInfoMy() {
+  return (
+    <>
+      <UserImg>
+        <Img
+          src="https://velog.velcdn.com/images/crg1050/profile/c180a703-e4c1-4c72-a014-9b7f0f3787a4/image.JPG"
+          alt="userImg" />
+      </UserImg>
+    </>
+  )
+}
---
+++ client/src/components/user/UserInfoMy.styled.jsx
@@ -0,0 +1,17 @@
+import tw from "tailwind-styled-components";
+
+export const UserImg = tw.div`
+  w-10
+  h-10
+  rounded-full
+  flex
+  item-center
+  overflow-hidden
+`
+
+export const Img = tw.img`
+  w-full
+  object-cover
+  rounded-full
+  object-cover
+`
