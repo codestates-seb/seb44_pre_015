545fc00f474486521375d80a43f8cf0c7eb1ed4a
jieun419 <crg1050@naver.com>
2023-06-20 09:58:52 +0900

    [add]:질문 작성 페이지 태그 섹션 스타일 작업 직문 작성 페이지 태그 섹션 스타일 및 배치 작업했습니다.

--- client/src/components/button/question/tagcheck/QuestionTagCheck.jsx
+++ client/src/components/button/question/tagcheck/QuestionTagCheck.jsx
@@ -1,6 +1,14 @@
-export default function QuestionTagCheck(){
-  return(
+import TagCheckBox from "../../../tag/TagCheckBox"
+import { QuestionTitWrap, QuestionTit, QuestionText } from "./QuestionTagCheck.styled"
+
+export default function QuestionTagCheck() {
+  return (
     <>
+      <QuestionTitWrap>
+        <QuestionTit>태그 선택</QuestionTit>
+        <QuestionText>최대 3개까지 선택 가능합니다</QuestionText>
+      </QuestionTitWrap>
+      <TagCheckBox />
     </>
   )
 }
--- client/src/components/button/question/tagcheck/QuestionTagCheck.styled.jsx
+++ client/src/components/button/question/tagcheck/QuestionTagCheck.styled.jsx
@@ -1,1 +1,18 @@
 import tw from "tailwind-styled-components";
+
+export const QuestionTitWrap = tw.div`
+  flex
+  flex-col
+  w-full
+  ml-48
+`;
+
+export const QuestionTit = tw.h2`
+  text-lg
+  font-bold
+`
+
+export const QuestionText = tw.p`
+  text-xs
+  text-slate-500
+`;
