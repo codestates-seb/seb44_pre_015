b88e0a83889e9b20a2a773b913f1beac6a5cc91c
hyeongu1 <gusrn556111@gmail.com>
2023-06-20 10:32:40 +0900

    [add]: 질문작성 제목 추가

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,12 @@ import Header from "./components/common/Header";
 import Footer from "./components/common/Footer";
 import Login from './pages/login/Login'
 import Main from './pages/main/Main'
-
+import QuestionWriteHead from "./components/button/question/questionwritehead/QuestionWriteHead";
 export default function App() {
   return (
     <BrowserRouter>
       <Header />
-      
+      <QuestionWriteHead></QuestionWriteHead>
       <Routes>
         <Route path='/' element={<Main />} />
         <Route path="/login" element={<Login />} />
