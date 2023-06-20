26ba5174432e44126c7d40d5f033ae471a1d23e8
jieun419 <crg1050@naver.com>
2023-06-20 16:02:37 +0900

    [add/style]:User Img 스타일 공통으로 관리하기 User Img 스타일 공통으로 관리할 수 있도록 코드 수정했습니다.

--- client/src/components/user/UserInfoMy.jsx
+++ client/src/components/user/UserInfoMy.jsx
@@ -1,15 +1,16 @@
-import { UserInfoMyContainer, UserInfoBox, UserImg, Img, InfoData, SmTit, NameEdit, UserInfo, NameEditBtn } from "./UserInfoMy.styled"
+import { UserImgMd, Img } from "./UserCommon.styled"
+import { UserInfoMyContainer, UserInfoBox, InfoData, SmTit, NameEdit, UserInfo, NameEditBtn } from "./UserInfoMy.styled"
 import { BsPencilFill } from "react-icons/bs"
 import { FcGoogle } from "react-icons/fc"
 
 export default function UserInfoMy() {
   return (
     <UserInfoMyContainer>
-      <UserImg>
+      <UserImgMd>
         <Img
           src="https://velog.velcdn.com/images/crg1050/profile/c180a703-e4c1-4c72-a014-9b7f0f3787a4/image.JPG"
           alt="userImg" />
-      </UserImg>
+      </UserImgMd>
 
       <UserInfoBox>
 
--- client/src/components/user/UserInfoMy.styled.jsx
+++ client/src/components/user/UserInfoMy.styled.jsx
@@ -1,21 +1,5 @@
 import tw from "tailwind-styled-components";
 
-export const UserImg = tw.div`
-  w-28
-  h-28
-  rounded-full
-  flex
-  item-center
-  overflow-hidden
-`
-
-export const Img = tw.img`
-  w-full
-  object-cover
-  rounded-full
-  object-cover
-`
-
 export const UserInfoMyContainer = tw.section`
   flex
   gap-5
