818a7f571c40cde4054d8d6dfb0fbf30628b51f4
yeon0226 <w01086248649@gmail.com>
2023-06-20 15:47:12 +0900

    [add] : mypage - question 카드 섹션 구현 mypage에 본인이 작성한 글에 대한 question 카드 섹션부분을 구현하였습니다.

---
+++ client/src/components/card/QuestionCard.styled.jsx
@@ -0,0 +1,35 @@
+import tw from 'tailwind-styled-components';
+
+export const CardWrapper = tw.div`
+  w-1/3
+  border
+  border-solid
+  border-[#DCDCDC]
+  rounded-lg
+  p-3
+  cursor-pointer
+  hover:border-ff7e22
+  duration-300
+  max-sm:w-[30rem]
+  m-4
+`
+
+export const CardTitle = tw.h3`
+  mt-2
+  text-sm
+  font-bold
+  text-[#101010]
+`
+
+export const CardContent = tw.p`
+  mt-2
+  text-xs
+  text-[#747474]
+`
+
+export const SelectWrapper = tw.div`
+  flex
+  justify-end
+  text-xs
+  mt-2
+`
