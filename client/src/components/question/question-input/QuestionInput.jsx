1b96c57e2b65a1ff5a29229dda1c0e3f05e2534b
ddaeunbb <ddaeunbb@gmail.com>
2023-06-20 11:23:48 +0900

    []

---
+++ client/src/components/question/question-input/QuestionInput.jsx
@@ -0,0 +1,9 @@
+import tw from 'tailwind-styled-components'
+import QuestionWriteHead from '../../button/question/questionwritehead/QuestionWriteHead'
+
+
+export default function QuestionInput() {
+  return (
+    <div>QuestionInput</div>
+  )
+}
