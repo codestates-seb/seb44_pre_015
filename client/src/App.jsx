4d1fb853e597b4e5692c4a470e9347b0e3192fa9
ddaeunbb <ddaeunbb@gmail.com>
2023-06-20 17:41:18 +0900

    [add]: 마이페이지 pull request 추합 완료

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,18 +5,19 @@ import Login from './pages/login/Login'
 import Main from './pages/main/Main'
 import Detail from './pages/detail/Detail'
 import Question from './pages/question/Question'
+import Mypage from './pages/mypage/Mypage';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Header />
 
-
       <Routes>
         <Route path='/' element={<Main />} />
         <Route path="/login" element={<Login />} />
         <Route path='/post:id' element={<Detail />} />
         <Route path='/question' element={<Question />}/>
+        <Route path="/mypage" element={<Mypage />} />
       </Routes>
 
       <Footer />
