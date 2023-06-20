3040c95cfd1ca0f8d2b6be077ace5fb532cdab7f
hyeongu1 <gusrn556111@gmail.com>
2023-06-20 09:37:48 +0900

    [remove]: 겹치는 파일 삭제
    question폴더 밖에 파일하나 더 있어서 삭제 했습니다

---
+++ client/src/components/button/question/questionwritehead/OrangeCircle.jsx
@@ -0,0 +1,11 @@
+import { QuestionWrap, QuestionHead, QuestionHeadWrap, OrangeCircle,QuestionHeadtext } from "../QuestionWriteHead.styled";
+
+
+export default function QuestionWriteHead () {
+    return(
+        <QuestionHeadWrap>
+            <OrangeCircle />
+        </QuestionHeadWrap>
+        
+    )
+}
