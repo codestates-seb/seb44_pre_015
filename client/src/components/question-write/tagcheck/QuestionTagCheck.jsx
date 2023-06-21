d0d38af5863a2f0621259d7ba98f25f9e5afb6e7
jieun419 <crg1050@naver.com>
2023-06-21 15:23:02 +0900

    [style/feat]:체크 박스 3개만 체크 할 수 있도록 수정 및 코드 1차 정리 체크 박스 3개만 체크할 수 있도록 작업 및 코드 1차 정리 했습니다.

--- client/src/components/question-write/tagcheck/QuestionTagCheck.jsx
+++ client/src/components/question-write/tagcheck/QuestionTagCheck.jsx
@@ -1,20 +1,29 @@
 import TagCheckBox from "../../tag/TagCheckBox"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { QuestionTitWrap, QuestionTit, QuestionText } from "./QuestionTagCheck.styled"
 
 export default function QuestionTagCheck() {
-  const [tags, setTags] = useState([])
+  const [tags, setTags] = useState([]);
+  const [checkCount, setCheckCount] = useState(0)
 
   const handlerTag = (name, description) => {
     let newTag = [...tags, { tagName: name, tagDescription: description }]
     let filterTag = tags.filter((el) => el.tagName !== name)
+    let tagSelected = tags.some(el => el.tagName === name)
 
-    if (tags.some(el => el.tagName === name)) {
+    if (tagSelected) {
       setTags(filterTag)
+      setCheckCount(checkCount-1)
     } else {
       setTags(newTag)
+      setCheckCount(checkCount+1)
     }
   }
+
+  useEffect(() => {
+    console.log(tags)
+    console.log(checkCount)
+  }, [tags])
 
   return (
     <>
@@ -22,7 +31,7 @@ export default function QuestionTagCheck() {
         <QuestionTit>태그 선택</QuestionTit>
         <QuestionText>최대 3개까지 선택 가능합니다</QuestionText>
       </QuestionTitWrap>
-      <TagCheckBox handlerTag={handlerTag} tags={tags}/>
+      <TagCheckBox handlerTag={handlerTag} tags={tags} checkCount={checkCount}/>
     </>
   )
 }
--- client/src/components/tag/TagCheckBox.jsx
+++ client/src/components/tag/TagCheckBox.jsx
@@ -1,6 +1,6 @@
 import { TagCheckContainer, TagCheck, TagCheckBoxed, TagLabel } from "./TagCheckBox.styled"
 
-export default function TagCheckBox({ handlerTag, tags}) {
+export default function TagCheckBox({ handlerTag, tags, checkCount }) {
   const tagList =
     [
       {
@@ -31,16 +31,19 @@ export default function TagCheckBox({ handlerTag, tags}) {
     <TagCheckContainer>
 
       {
-        tagList.map((el, idx) => (
-          <TagCheckBoxed key={idx}>
-            <TagCheck 
-            type="checkbox"
-            id={`${el.tagName}-${idx}`}
-            value={el.tagName}
-            checked={tags.some(item => item.tagName === el.tagName)}
-            onChange={() => handlerTag(el.tagName, el.tagDescription)}
+        tagList.map((el) => (
+          <TagCheckBoxed key={el.tagName}>
+            <TagCheck
+              type="checkbox"
+              id={el.tagName}
+              value={el.tagName}
+              checked={tags.some(item => item.tagName === el.tagName)}
+              disabled={!tags.some(item => item.tagName === el.tagName) && checkCount >= 3}
+              onChange={() => handlerTag(el.tagName, el.tagDescription)}
             ></TagCheck>
-            <TagLabel htmlFor={`${el.tagName}-${idx}`}><span>{el.tagName}</span></TagLabel>
+            <TagLabel htmlFor={el.tagName}>
+              <span>{el.tagName}</span>
+            </TagLabel>
           </TagCheckBoxed>
         ))
       }
