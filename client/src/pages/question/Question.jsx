176fe2596c1c479405ac679d75026d75fd450d8b
yeon0226 <w01086248649@gmail.com>
2023-06-21 17:00:27 +0900

    [feat] 기능 추합 태그 기능과 입력 기능해서 메인으로 전달하는 기능 추합하였습니다.

--- client/src/pages/question/Question.jsx
+++ client/src/pages/question/Question.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState } from 'react';
 import axios from 'axios';
 import { QuestionContainer } from './Question.styled';
 import QuestionWriteHead from '../../components/question-write/questionwritehead/QuestionWriteHead';
@@ -12,22 +12,22 @@ export default function Question() {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
   const [tags, setTags] = useState([]);
-  const [checkCount, setCheckCount] = useState(0)
+  const [checkCount, setCheckCount] = useState(0);
   const navigate = useNavigate();
 
   const handlerTag = (name, description) => {
-    let newTag = [...tags, { tagName: name, tagDescription: description }]
-    let filterTag = tags.filter((el) => el.tagName !== name)
-    let tagSelected = tags.some(el => el.tagName === name)
+    const tagSelected = tags.some((el) => el.tagName === name);
 
     if (tagSelected) {
-      setTags(filterTag)
-      setCheckCount(checkCount - 1)
+      const updatedTags = tags.filter((el) => el.tagName !== name);
+      setTags(updatedTags);
+      setCheckCount((prevCount) => prevCount - 1);
     } else {
-      setTags(newTag)
-      setCheckCount(checkCount + 1)
+      const newTag = { tagName: name, tagDescription: description };
+      setTags((prevTags) => [...prevTags, newTag]);
+      setCheckCount((prevCount) => prevCount + 1);
     }
-  }
+  };
 
   const titleChange = (e) => {
     setTitle(e.target.value);
@@ -42,7 +42,7 @@ export default function Question() {
       title: title,
       detail: content,
       memberId: 1,
-      tags: tags
+      tags: [1, 2, 3]
     };
 
     const headers = {
@@ -50,42 +50,30 @@ export default function Question() {
       'ngrok-skip-browser-warning': true,
     };
 
-    const submittedTitle = requestData.title; 
-    const submittedDetail = requestData.detail; 
-
     axios
-      .post('https://56d7-59-11-30-105.ngrok-free.app/questions/register', requestData, { headers })
-      .then((response) => {
-        console.log('질문 등록 성공');
-        console.log('등록된 질문 정보:');
-        console.log(JSON.stringify({
-          title: submittedTitle,
-          detail: submittedDetail,
-          ...response.data
-        }, null, 2));
-
-        clearInput(); 
-        navigate('/');
-  
-      })
-      .catch((error) => {
-        console.log('에러:', error);
-      });
+    .post('https://56d7-59-11-30-105.ngrok-free.app/questions/register', requestData, { headers })
+    .then((response) => {
+      clearInput();
+      navigate('/');
+    })
+    .catch((error) => {
+      console.log('에러:', error);
+    });
   };
 
   const clearInput = () => {
     setTitle('');
     setContent('');
+    setTags([]);
+    setCheckCount(0);
   };
-
-
 
   return (
     <QuestionContainer>
       <QuestionWriteHead />
       <QuestionTitle value={title} onChange={titleChange} />
       <QuestionInput value={content} onChange={contentChange} />
-      <QuestionTagCheck handlerTag={handlerTag} tags={tags} checkCount={checkCount}/>
+      <QuestionTagCheck handlerTag={handlerTag} tags={tags} checkCount={checkCount} />
       <AskBtn onClick={questionSubmit} />
     </QuestionContainer>
   );
