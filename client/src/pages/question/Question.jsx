a95ecfe0a7cf29813d59dec04ebabdfac222cdd9
yeon0226 <w01086248649@gmail.com>
2023-06-21 15:19:30 +0900

    [feat] : 질문페이지 - 질문등록 기능 구현 질문 페이지에서 제목과 본문에 값을 입력하고 질문하기 버튼을 누르면 등록이 되는 기능을 구현했습니다.

--- client/src/pages/question/Question.jsx
+++ client/src/pages/question/Question.jsx
@@ -1,18 +1,64 @@
-import { QuestionContainer } from './Question.styled'
-import QuestionWriteHead from '../../components/question-write/questionwritehead/QuestionWriteHead'
-import QuestionTitle from '../../components/question-write/question-title/QuestionTitle'
-import QuestionInput from '../../components/question-write/question-input/QuestionInput'
-import QuestionTagCheck from '../../components/question-write/tagcheck/QuestionTagCheck'
-import AskBtn from '../../components/button/askButton/AskBtn'
+import React, { useState } from 'react';
+import axios from 'axios';
+import { QuestionContainer } from './Question.styled';
+import QuestionWriteHead from '../../components/question-write/questionwritehead/QuestionWriteHead';
+import QuestionTitle from '../../components/question-write/question-title/QuestionTitle';
+import QuestionInput from '../../components/question-write/question-input/QuestionInput';
+import QuestionTagCheck from '../../components/question-write/tagcheck/QuestionTagCheck';
+import AskBtn from '../../components/button/askButton/AskBtn';
+
 
 export default function Question() {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
+
+  const titleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const contentChange = (e) => {
+    setContent(e.target.value);
+  };
+
+  const questionSubmit = () => {
+
+    const requestData = {
+      title: title,
+      detail: content,
+      memberId: 1,
+      tags: [1, 2, 3]
+    };
+
+
+    axios
+      .post('https://56d7-59-11-30-105.ngrok-free.app/questions/register', requestData)
+      .then((response) => {
+        console.log('질문 등록 성공');
+        console.log('등록된 질문 정보:');
+        console.log(response.data);
+
+
+
+        clearInput();
+      })
+      .catch((error) => {
+        console.log('에러:', error);
+      });
+  };
+
+  const clearInput = () => {
+    setTitle('');
+    setContent('');
+  };
+
   return (
     <QuestionContainer>
       <QuestionWriteHead />
-      <QuestionTitle/>
-      <QuestionInput />
+      <QuestionTitle value={title} onChange={titleChange} />
+      <QuestionInput value={content} onChange={contentChange} />
       <QuestionTagCheck />
-      <AskBtn />
+      <AskBtn onClick={questionSubmit} />
     </QuestionContainer>
-  )
+  );
 }
