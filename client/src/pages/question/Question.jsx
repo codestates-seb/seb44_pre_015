2a203192f915c0fdf9a29ce0db8c8029c3096bbe
yeon0226 <w01086248649@gmail.com>
2023-06-21 15:26:26 +0900

    [feat] 페이지 이동 기능 추가 질문하기 버튼을 눌렀을때 메인페이지로 넘어가도록 기능을 추가하였습니다.

--- client/src/pages/question/Question.jsx
+++ client/src/pages/question/Question.jsx
@@ -6,12 +6,12 @@ import QuestionTitle from '../../components/question-write/question-title/Questi
 import QuestionInput from '../../components/question-write/question-input/QuestionInput';
 import QuestionTagCheck from '../../components/question-write/tagcheck/QuestionTagCheck';
 import AskBtn from '../../components/button/askButton/AskBtn';
-
+import { useNavigate } from 'react-router-dom';
 
 export default function Question() {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
-
+  const navigate = useNavigate();
 
   const titleChange = (e) => {
     setTitle(e.target.value);
@@ -22,7 +22,6 @@ export default function Question() {
   };
 
   const questionSubmit = () => {
-
     const requestData = {
       title: title,
       detail: content,
@@ -30,17 +29,28 @@ export default function Question() {
       tags: [1, 2, 3]
     };
 
+    const headers = {
+      'Content-Type': 'application/json',
+      'ngrok-skip-browser-warning': true,
+    };
+
+    const submittedTitle = requestData.title; 
+    const submittedDetail = requestData.detail; 
 
     axios
-      .post('https://56d7-59-11-30-105.ngrok-free.app/questions/register', requestData)
+      .post('https://56d7-59-11-30-105.ngrok-free.app/questions/register', requestData, { headers })
       .then((response) => {
         console.log('질문 등록 성공');
         console.log('등록된 질문 정보:');
-        console.log(response.data);
+        console.log(JSON.stringify({
+          title: submittedTitle,
+          detail: submittedDetail,
+          ...response.data
+        }, null, 2));
 
-
-
-        clearInput();
+        clearInput(); 
+        navigate('/');
+  
       })
       .catch((error) => {
         console.log('에러:', error);
@@ -51,6 +61,8 @@ export default function Question() {
     setTitle('');
     setContent('');
   };
+
+
 
   return (
     <QuestionContainer>
