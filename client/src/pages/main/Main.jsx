4fc90e6a1f4de2211937efe8efbdfabcbacbc6be
hyeongu1 <gusrn556111@gmail.com>
2023-06-21 19:57:34 +0900

    [add]: 무한스크롤 기능 추가 스크롤 내릴때마다 3개씩 늘어나게 구현

--- client/src/pages/main/Main.jsx
+++ client/src/pages/main/Main.jsx
@@ -3,33 +3,47 @@ import SearchBox from '../../components/search/SearchBox';
 import FilterBtn from '../../components/filter/FilterBtn';
 import MainPostCard from '../../components/card/MainPostCard';
 import { useEffect, useState } from 'react';
+import { useInView } from 'react-intersection-observer';
 import axios from 'axios';
 
 export default function Main() {
   const [postCardData, setPostCardData] = useState([]);
   const [filteredData, setFilteredData] = useState([]);
   const [searchQuery, setSearchQuery] = useState('');
+  const [page, setPage] = useState(0);
+  const [ref, inView] = useInView();
 
   useEffect(() => {
     getApi();
   }, []);
 
+  useEffect(() => {
+    if (inView) {
+      loadMore();
+    }
+  }, [inView]);
+
   const getApi = async () => {
     try {
       const response = await axios.get(
-        'https://56d7-59-11-30-105.ngrok-free.app/questions/recent?page=0&size=100',
+        `http://ec2-13-125-172-34.ap-northeast-2.compute.amazonaws.com:8080/questions/recent?page=${page}&size=3`,
         {
           headers: {
-            'Content-Type': 'application/json',
-            'ngrok-skip-browser-warning': '69420',
+            'Content-Type': 'application/json;charset=UTF-8',
+            Accept: 'application/json',
           },
         }
       );
-      setPostCardData(response.data);
-      setFilteredData(response.data);
+      setPostCardData((prevData) => [...prevData, ...response.data]);
+      setFilteredData((prevData) => [...prevData, ...response.data]);
+      setPage((prevPage) => prevPage + 1);
     } catch (error) {
       console.log(error);
     }
+  };
+
+  const loadMore = () => {
+    getApi();
   };
 
   const handleFilter = (filter) => {
@@ -59,19 +73,38 @@ export default function Main() {
       <SearchBox handleSearch={handleSearch} />
       <FilterBtn handleFilter={handleFilter} />
 
-      {filteredData.map((data) => (
-        <MainPostCard
-          key={data.questionId}
-          title={data.title}
-          content={data.detail}
-          votesCount={data.votesCount}
-          viewCount={data.viewCount}
-          answerCount={data.answerCount}
-          createdAt={data.createdAt}
-          memberId={data.memberId}
-          tags={data.tags}
-        />
-      ))}
+      {filteredData.map((data, index) => {
+        if (index === filteredData.length - 1 && index % 3 === 2) {
+          return (
+            <div ref={ref} key={data.questionId}>
+              <MainPostCard
+                title={data.title}
+                content={data.detail}
+                votesCount={data.votesCount}
+                viewCount={data.viewCount}
+                answerCount={data.answerCount}
+                createdAt={data.createdAt}
+                memberId={data.memberId}
+                tags={data.tags}
+              />
+            </div>
+          );
+        } else {
+          return (
+            <MainPostCard
+              key={data.questionId}
+              title={data.title}
+              content={data.detail}
+              votesCount={data.votesCount}
+              viewCount={data.viewCount}
+              answerCount={data.answerCount}
+              createdAt={data.createdAt}
+              memberId={data.memberId}
+              tags={data.tags}
+            />
+          );
+        }
+      })}
     </MainContainer>
   );
 }
