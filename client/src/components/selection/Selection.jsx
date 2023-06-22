d4da78fa9d0a383a74b91b6ae49a0b133f9fff9b
ddaeunbb <ddaeunbb@gmail.com>
2023-06-22 18:44:17 +0900

    [add]: 메인페이지 mainSlice 생성 코드가 가독성이 떨어지게되어 redux를 활용하게 되었습니다.

--- client/src/components/selection/Selection.jsx
+++ client/src/components/selection/Selection.jsx
@@ -1,11 +1,12 @@
 import { AiOutlineCheck } from "react-icons/ai";
 import { SelectionContainer, SelectionText } from './Selection.styled';
 
-export default function Selection({ solutionStatus }) {
+export default function Selection({ status }) {
+
   return (
     <SelectionContainer>
-      <AiOutlineCheck style={{ color: solutionStatus ? "#03B800" : "#DCDCDC" }} />
-      <SelectionText solutionStatus={solutionStatus}>selection</SelectionText>
+      <AiOutlineCheck style={{ color: status==='true' ? "#03B800" : "#DCDCDC" }} />
+      <SelectionText status={status}>selection</SelectionText>
     </SelectionContainer>
   );
 }
--- client/src/components/selection/Selection.styled.jsx
+++ client/src/components/selection/Selection.styled.jsx
@@ -5,5 +5,6 @@ export const SelectionContainer = tw.div`
 `;
 
 export const SelectionText = tw.span`
-  ${({solutionStatus}) => solutionStatus ? "text-[#03B800]" :"text-[#DCDCDC]" }
+  text-[#DCDCDC]
+  ${({status}) => status==='true' && "text-[#03B800]" }
 `;
