94e2343128cb478434e41c35f6ec3eac53153778
ddaeunbb <ddaeunbb@gmail.com>
2023-06-22 15:08:37 +0900

    [fix]: recommnetSlice 삭제 기능 구현에 있어 redux가 필요하지 않을 것 같아 recommentSlice가 필요하지 않을 것 같아 파일을 삭제하였습니다.

--- client/src/modules/index.js
+++ client/src/modules/index.js
@@ -1,11 +1,9 @@
 import { configureStore }  from '@reduxjs/toolkit';
 import questionSlice from './questionSlice'
-import recommentSlice from './recommnetSlice';
 
 const store = configureStore({
   reducer: {
     question: questionSlice.reducer,
-    recomment: recommentSlice.reducer,
   }
 });
 
