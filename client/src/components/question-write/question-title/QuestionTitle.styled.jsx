c2eaaf2984b74a07f28c583a3080f998c9275f81
yeon0226 <w01086248649@gmail.com>
2023-06-22 19:24:14 +0900

    [style] 스타일 수정

--- client/src/components/question-write/question-title/QuestionTitle.styled.jsx
+++ client/src/components/question-write/question-title/QuestionTitle.styled.jsx
@@ -1,23 +1,20 @@
 import tw from 'tailwind-styled-components';
 
-
 export const QWrapper = tw.div`
-flex
-flex-col
-`
-export const TContainer = tw.div`
+  flex
+  flex-col
+`;
+export const QInputContainer = tw.div`
   w-1/2
   flex
   items-center
-`
-
+`;
 export const QTitle = tw.h3`
   ml-2
   font-bold
   text-lg
-`
-
-export const Qinput = tw.input`
+`;
+export const QInput = tw.input`
   w-[40rem]
   max-sm:w-[29rem]
   bg-white
@@ -27,4 +24,4 @@ export const Qinput = tw.input`
   py-3
   px-4
   text-sm
-`
+`;
