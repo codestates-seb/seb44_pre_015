3215b11218f0ee447b47fc48ed4f8fed8aa6bfe2
yeon0226 <w01086248649@gmail.com>
2023-06-22 19:33:28 +0900

    [refactor] redux 리팩토링 질문 등록페이지의 title 제목 부분의 기능을 redux로 리팩토링했습니다.

--- client/src/components/question-write/question-title/QuestionTitle.jsx
+++ client/src/components/question-write/question-title/QuestionTitle.jsx
@@ -1,10 +1,17 @@
+import { typeTitle } from '../../../modules/questionSlice';
 import OrangeCircle from '../questionwritehead/OrangeCircle';
 import { QTitle, QWrapper, QInputContainer, QInput } from './QuestionTitle.styled';
+import { useDispatch, useSelector } from 'react-redux';
 
-export default function QuestionTitle(props) {
-  const { value, onChange } = props;
+export default function QuestionTitle() {
+  const title = useSelector((state) => state.question.title);
+  const dispatch = useDispatch();
+
+  const handleChange = (e) => {
+    dispatch(typeTitle(e.target.value));
+  };
+
   const maxLength = 80;
-
 
   return (
     <QWrapper>
@@ -12,12 +19,12 @@ export default function QuestionTitle(props) {
         <OrangeCircle />
         <QTitle>제목</QTitle>
       </QInputContainer>
-    
+
       <QInput
         type='text'
         placeholder='제목을 작성해 주세요. (80자 이하)'
-        value={value}
-        onChange={onChange}
+        value={title}
+        onChange={handleChange}
         maxLength={maxLength}
       />
     </QWrapper>
