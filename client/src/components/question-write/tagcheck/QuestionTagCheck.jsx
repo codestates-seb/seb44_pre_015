7ae4d18b8399202ae7d38690f5717700163b3adc
jieun419 <crg1050@naver.com>
2023-06-23 17:10:52 +0900

    [style/remove]:태그 기능 구현 및 더미 데이터 삭제 API 명세서에 맞춰 태크 수정 및 기존 더미 데이터 삭제

--- client/src/components/question-write/tagcheck/QuestionTagCheck.jsx
+++ client/src/components/question-write/tagcheck/QuestionTagCheck.jsx
@@ -1,14 +1,14 @@
 import TagCheckBox from "../../tag/TagCheckBox"
 import { QuestionTitWrap, QuestionTit, QuestionText } from "./QuestionTagCheck.styled"
 
-export default function QuestionTagCheck({handlerTag, tags, checkCount}) {
+export default function QuestionTagCheck({handlerTag, tags, checkCount, accessToken}) {
   return (
     <>
       <QuestionTitWrap>
         <QuestionTit>태그 선택</QuestionTit>
         <QuestionText>최대 3개까지 선택 가능합니다</QuestionText>
       </QuestionTitWrap>
-      <TagCheckBox handlerTag={handlerTag} tags={tags} checkCount={checkCount} />
+      <TagCheckBox handlerTag={handlerTag} tags={tags} checkCount={checkCount} accessToken={accessToken} />
     </>
   )
 }
