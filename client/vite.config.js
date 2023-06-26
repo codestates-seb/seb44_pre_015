9dc348e1ec470a5a9708f696b26839640855fe27
ddaeunbb <ddaeunbb@gmail.com>
2023-06-26 15:07:12 +0900

    [add]: cofig파일 수정

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "seb44_pre_015",
   plugins: [react()],
-  // server: {
-  //   proxy: {
-  //     '/login': 'http://ec2-13-125-172-34.ap-northeast-2.compute.amazonaws.com:8080',
-  //   },
-  // }
 })
