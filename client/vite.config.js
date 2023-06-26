efd48f81ea3171e8bb53d5e3f8eba3fdbccf6e52
ddaeunbb <ddaeunbb@gmail.com>
2023-06-26 15:43:54 +0900

    [fix]: config 파일 수정

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "seb44_pre_015",
+  base: "/seb44_pre_015/",
   plugins: [react()],
 })
