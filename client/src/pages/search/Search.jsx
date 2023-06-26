418361ae6a1270dfb69d7562a111d6b1bc8460bd
ddaeunbb <ddaeunbb@gmail.com>
2023-06-26 14:00:43 +0900

    [fix]: 검색이 한번만 되는 bug-fix 검색창에 검색 이후, 또 검색을 했을 경우 검색이 되지 않아서 라우팅을 나눠 검색마다 새로운 페이지를 그리도록 하였습니다.

--- client/src/pages/search/Search.jsx
+++ client/src/pages/search/Search.jsx
@@ -1,28 +1,27 @@
 import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { filterData } from '../../modules/searchSlice';
 import axios from 'axios';
-import { useInView } from 'react-intersection-observer';
 import { MainContainer } from '../main/Main.styled'
 import SearchBox from '../../components/search/SearchBox'
-import FilterBtn from '../../components/filter/FilterBtn'
 import MainPostCard from '../../components/card/MainPostCard'
+
 
 
 export default function Search() {
   const dispatch = useDispatch();
+  const { searchWord } = useParams();
   const datas = useSelector(state => state.search.searchData);
-
 
   useEffect(()=>{
     axios(`http://ec2-13-125-172-34.ap-northeast-2.compute.amazonaws.com:8080/questions/get/recent?page=0&size=100`)
     .then(res => dispatch(filterData(res.data)));
-  }, [])
+  }, [searchWord])
 
     return(
       <MainContainer>
         <SearchBox />
-        <FilterBtn />
           {
             datas.map(data => <MainPostCard key={data.questionId} title={data.title} detail={data.detail} status={data.solutionStatus.toString()} viewCount={data.viewCount} votesCount={data.votesCount} answerCount={data.answerCount} questionId={data.questionId} createdAt={data.createdAt} memberInfo={data.memberInfoDto}/>)
           }
