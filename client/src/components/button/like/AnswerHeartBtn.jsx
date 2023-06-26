c260b368ee41171268d84f9b4a10fd9a5a2c49ba
ddaeunbb <ddaeunbb@gmail.com>
2023-06-26 11:27:25 +0900

    [add]: 답변 좋아요 기능 구현 완료

--- client/src/components/button/like/AnswerHeartBtn.jsx
+++ client/src/components/button/like/AnswerHeartBtn.jsx
@@ -1,14 +1,45 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 import { HeartButtonWrap } from "./HeartBtn.styled";
 import { AiFillHeart } from 'react-icons/ai';
 
-export default function AnswerHeartBtn() {
+export default function AnswerHeartBtn({votesCount, answerVoteByMember, answerId}) {
+  const navigate = useNavigate();
+  const [isVoted, setIsVoted]= useState(false);
+  const [totalCount, setTotalCount]= useState(0);
+
+  useEffect(()=> {
+    setIsVoted(answerVoteByMember);
+    setTotalCount(votesCount)
+  }, [answerVoteByMember, votesCount])
+
+  const onClickHandler = () => {
+    const isLogIn = JSON.parse(localStorage.getItem('isLogIn'));
+    if (!isLogIn) return alert('회원만 좋아요를 누를 수 있습니다.');
+    
+    const UID = JSON.parse(localStorage.getItem('UID'));
+    const accessToken = JSON.parse(localStorage.getItem('accessToken'));
+    
+    axios(`http://ec2-13-125-172-34.ap-northeast-2.compute.amazonaws.com:8080/answers/votes/${answerId}/${UID}`, {
+      headers: {
+        'Content-Type': 'application/json;charset=UTF-8',
+        'Authorization': `Bearer ${accessToken}`,
+      }
+    })
+    .then(res => {
+      setIsVoted(res.data.answerVoteStatus);
+      setTotalCount(res.data.totalVoteCount);
+    })
+    .catch(err => navigate('/*'));
+  }
 
   return (
     <div>
       <HeartButtonWrap className='mt-3'>
-        <AiFillHeart className={true ? "text-orange-500" : "text-gray-300"} style={{fontSize : '13px'}}/>
+        <AiFillHeart className={isVoted ? "text-orange-500" : "text-gray-300"} style={{fontSize : '13px'}} onClick={onClickHandler}/>
       </HeartButtonWrap>
-      <p className="ml-2.5 text-xs text-[#797979]">00</p>
+      <p className="ml-2.5 text-xs text-[#797979]">{totalCount}</p>
     </div>
   );
 }
